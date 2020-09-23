63a09ee888dbcebf5284d6c3d08dbac3e73c6462
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2020-09-23 22:32:21 +0900

    Merge

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,5 +1,25 @@
 import $ from 'jquery';
 
-console.log($('h1'));
+const searchOpenIcon = document.querySelector('.header-icon-item.search');
+const searchContainer = document.querySelector('.search-container');
+const searchCloseIcon = document.querySelector('.search-close-icon');
 
-console.log(1);
+const burgerMenuOpenIcon = document.querySelector('.header-icon-item.burger');
+const burgerMenuContainer = document.querySelector('.burger-menu-container');
+const burgerMenuCloseIcon = document.querySelector('.burger-menu-close-icon');
+
+searchOpenIcon.addEventListener('click', function () {
+  searchContainer.classList.remove('hidden');
+});
+
+searchCloseIcon.addEventListener('click', function () {
+  searchContainer.classList.add('hidden');
+});
+
+burgerMenuOpenIcon.addEventListener('click', function () {
+  burgerMenuContainer.classList.remove('hidden');
+});
+
+burgerMenuCloseIcon.addEventListener('click', function () {
+  burgerMenuContainer.classList.add('hidden');
+});
