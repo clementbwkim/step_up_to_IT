36d80200e6c6fd3ac5468efe22729b0af34f3f4f
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2020-10-12 19:41:50 +0900

    Modify master

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -9,79 +9,76 @@ var screenSize = {
   tablet: 768,
   small: 546,
 };
-const headerContainer = document.querySelector('.header-container');
-const searchOpenIcon = document.querySelector('.header-icon-item.search');
-const searchContainer = document.querySelector('.search-container');
-const searchCloseIcon = document.querySelector('.search-close-icon');
-const searchInputBar = document.querySelector('.search-input-bar');
 
-const burgerMenuOpenIcon = document.querySelector('.header-icon-item.burger');
-const burgerMenuContainer = document.querySelector('.burger-menu-container');
-const burgerMenuCloseIcon = document.querySelector('.burger-menu-close-icon');
-const logo = document.querySelector('.header-logo-items');
+/*Elemnets */
+let headerContainerEl;
+let searchOpenIconEl;
+let searchContainerEl;
+let searchCloseIconEl;
+let searchInputBarEl;
+let burgerMenuOpenIconEl;
+let burgerMenuContainerEl;
+let burgerMenuCloseIconEl;
+let logoEl;
 
+let scrollMax = 95;    
+let scaleX = 3.5;
+let scaleY = 3.5;
+let translateY = 112;
 
-function searchHiddenOff() {
-  searchContainer.classList.remove('hidden');
-  searchInputBar.focus();
+/*Functions */
+function searchHiddenOff () {
+  searchContainerEl.classList.remove('hidden');
+  searchInputBarEl.focus();
 }
-function searchHiddenAdd() {
-  searchContainer.classList.add('hidden');
+function searchHiddenAdd () {
+  searchContainerEl.classList.add('hidden');
 }
-function burgerHiddenOff() {
-  burgerMenuContainer.classList.remove('hidden');
+function burgerHiddenOff () {
+  burgerMenuContainerEl.classList.remove('hidden');
 }
-function burgerHiddenAdd() {
-  burgerMenuContainer.classList.add('hidden');
+function burgerHiddenAdd () {
+  burgerMenuContainerEl.classList.add('hidden');
+}
+function handleLogo () {  
+  let calcScaleX = scaleX - (scaleX / scrollMax * window.pageYOffset);
+  let calcScaleY = scaleY - (scaleY / scrollMax * window.pageYOffset);
+  let calcTranslateY = translateY - (translateY / scrollMax * window.pageYOffset);
+  logoEl.style.transform = `matrix(${calcScaleX > 1 ? calcScaleX : 1}, 0,0,${calcScaleY  > 1 ? calcScaleY : 1},0,${calcTranslateY  > 0 ? calcTranslateY : 0})`;
 }
 
-    const scrollMax = 95;    
-    let scaleX = 3.5;
-    let scaleY = 3.5;
-    let translateY = 112;
+function resize () {
+  window.addEventListener('resize', () => {
+    if(window.innerWidth <= screenSize.large) {
+      logoEl.style.transform = `matrix(1, 0, 0, 1, 0, 0)`;
+      logoEl.style.transition = `all .3s ease`;
+      window.removeEventListener('scroll', handleLogo);
+    }else{
+      if(scrollY <= scrollMax || pageYOffset <= scrollMax){
+        logoEl.style.transform = `matrix(${scaleX},0,0,${scaleY},0,${translateY})`;
+      }    
+      window.addEventListener('scroll', handleLogo);
+    }
+  });
+}
 
-  const handleLogo = () => {  
-    let calcScaleX = scaleX - (scaleX / scrollMax * window.scrollY);
-    let calcScaleY = scaleY - (scaleY / scrollMax * window.scrollY);
-    let calcTranslateY = translateY - (translateY / scrollMax * window.scrollY);
-    logo.style.transform = `matrix(${calcScaleX > 1 ? calcScaleX : 1}, 0,0,${calcScaleY  > 1 ? calcScaleY : 1},0,${calcTranslateY  > 0 ? calcTranslateY : 0})`;
-  }
-
-searchOpenIcon.addEventListener('click', searchHiddenOff);
-searchCloseIcon.addEventListener('click', searchHiddenAdd);
-burgerMenuOpenIcon.addEventListener('click', burgerHiddenOff);
-burgerMenuCloseIcon.addEventListener('click', burgerHiddenAdd);
-window.addEventListener('scroll', handleLogo);
-window.addEventListener('resize', () => {
-  if(window.innerWidth <= screenSize.large) {
-    logo.style.transform = `matrix(1, 0, 0, 1, 0, 0)`;
-    logo.style.transition = `all .3s ease`;
-    window.removeEventListener('scroll', handleLogo);
-  }else{
-    if(scrollY <= scrollMax || pageYOffset <= scrollMax){
-      logo.style.transform = `matrix(${scaleX},0,0,${scaleY},0,${translateY})`;
-    }    
-    window.addEventListener('scroll', handleLogo);
-  }
-});
-
-const handleLatestSection = () => {
+function handleLatestSection () {
   const latestContainer = document.querySelector('.latest-stories-container');
   const latestContentBox = document.querySelector('.latest-stories-content-box');
   const latestContentCover = document.querySelector('.latest-stories-content.cover');
   const latestContentSpacingCover = document.querySelector('.latest-stories-content.spacing-cover');
 
-  const enterAt = window.scrollY + window.innerHeight >= latestContainer.offsetTop + latestContentBox.clientHeight;
+  const enterAt = window.pageYOffset + window.innerHeight >= latestContainer.offsetTop + latestContentBox.clientHeight;
   const latestContainerBottomY = latestContainer.offsetTop + latestContainer.clientHeight;
   const marginBottom = 100;
   const sidePadding = 40;
-  const latestStoryBottomY = latestContainerBottomY - headerContainer.clientHeight - marginBottom;
-  const afterBottom = window.scrollY + window.innerHeight >= latestStoryBottomY;
+  const latestStoryBottomY = latestContainerBottomY - headerContainerEl.clientHeight - marginBottom;
+  const afterBottom = window.pageYOffset + window.innerHeight >= latestStoryBottomY;
 
   if (enterAt) {
     if (afterBottom) {
       latestContentCover.style.position = 'absolute';
-      latestContentCover.style.bottom = `${headerContainer.clientHeight + marginBottom}px`; // 수정 필요
+      latestContentCover.style.bottom = `${headerContainerEl.clientHeight + marginBottom}px`; // 수정 필요
       latestContentCover.style.width = '50%';
       latestContentSpacingCover.classList.remove('hidden');
     } else {
@@ -95,13 +92,8 @@ const handleLatestSection = () => {
     latestContentCover.style.width = '50%';
     latestContentSpacingCover.classList.add('hidden');
   }
-
 }
-
-window.addEventListener('scroll', handleLatestSection);
-
-
-$(function () {
+function currentIssueSlick () {
   $('.current-issues-contents').slick({
     centerMode: true,
     infinite: true,
@@ -130,27 +122,66 @@ $(function () {
       },
     ],
   });
-  if (window.innerWidth === screenSize.large) {
-    $('.selected-stories-content').slick({
-      centerMode: true,
-      infinite: true,
-      centerPadding: '10%',
-      slidesToShow: 3,
-      speed: 500,
-      variableWidth: false,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 545,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            slidesToShow: 2,
+}
+  function selectedStorySlick (){
+    if (window.innerWidth === screenSize.large) {
+      $('.selected-stories-content').slick({
+        centerMode: true,
+        infinite: true,
+        centerPadding: '10%',
+        slidesToShow: 3,
+        speed: 500,
+        variableWidth: false,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 545,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              slidesToShow: 2,
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
+    }
   }
-});
+  
+
+init();
+function init () {
+  initElements();
+  initEvents();
+}
+
+function initEvents () {
+  searchOpenIconEl.addEventListener('click', searchHiddenOff);
+  searchCloseIconEl.addEventListener('click', searchHiddenAdd);
+  burgerMenuOpenIconEl.addEventListener('click', burgerHiddenOff);
+  burgerMenuCloseIconEl.addEventListener('click', burgerHiddenAdd);
+  window.addEventListener('scroll', handleLogo);
+  resize();
+  handleLatestSection ();
+  window.addEventListener('scroll', handleLatestSection);
+}
+
+function initElements () {
+  headerContainerEl = document.querySelector('.header-container');
+  searchOpenIconEl = document.querySelector('.header-icon-item.search');
+  searchContainerEl = document.querySelector('.search-container');
+  searchCloseIconEl = document.querySelector('.search-close-icon');
+  searchInputBarEl = document.querySelector('.search-input-bar');
+  burgerMenuOpenIconEl = document.querySelector('.header-icon-item.burger');
+  burgerMenuContainerEl = document.querySelector('.burger-menu-container');
+  burgerMenuCloseIconEl = document.querySelector('.burger-menu-close-icon');
+  logoEl = document.querySelector('.header-logo-items');
+}
+
+slickSlides();
+function slickSlides (){
+  currentIssueSlick();
+  selectedStorySlick();
+}
+
 
 
