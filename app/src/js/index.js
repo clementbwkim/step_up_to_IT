cb3d6fb8cb6e5266f523d9f138e526a181a8f695
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2020-10-16 02:29:09 +0900

    Modify master

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -9,18 +9,6 @@ $(function () {
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
     controlArrows: false,
-    afterLoad: function (anchorLink, index){
-			if (index == 1){
-				$('.intro-img').addClass('animation-on');
-			}			
-			else if (index == 2){
-				$('.img-box').addClass('animation-on');
-			}
-			else if (index ==3){
-				$('.contact-logo').addClass('animation-on');
-			}
-	
-		}
   });
 });
 
