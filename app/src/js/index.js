8fe1168730a985b0ed9d9c5bdfdd37390af39d50
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2020-11-12 19:58:59 +0900

    Add btn cross

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -2,20 +2,29 @@ import $ from 'jquery';
 
 let headerBtnEl;
 let headerContentsEl;
+let barsEl;
 
 init();
 function init() {
     initElements();
     showNavigation();
+    barsCross();
 }
 
 function initElements() {
     headerBtnEl = $('.btn-wrapper');
     headerContentsEl = $('.header-contents');
+    barsEl = $('.bars');
 }
 
 function showNavigation(){
     headerBtnEl.click(()=>{
         headerContentsEl.toggleClass('header-slide');
     });
+}
+
+function barsCross(){
+    barsEl.click(() => {
+        barsEl.toggleClass('cross');
+    })
 }
