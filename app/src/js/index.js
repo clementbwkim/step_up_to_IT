53983819075af22e2edf6ab689540accfcc14fc2
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2020-11-25 02:58:13 +0900

    Add anime index

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -3,18 +3,25 @@ import $ from 'jquery';
 let headerBtnEl;
 let headerContentsEl;
 let barsEl;
+let bannerTextEl;
+let bannerTextSpanEl;
+let bannerKeywordEl;
 
 init();
 function init() {
     initElements();
     showNavigation();
     barsCross();
+    addClassOn();
 }
 
 function initElements() {
     headerBtnEl = $('.btn-wrapper');
     headerContentsEl = $('.header-contents');
     barsEl = $('.bars');
+    bannerTextEl = $('.banner-text h2');
+    bannerTextSpanEl = $('.banner-text h2 span')
+    bannerKeywordEl = $('.banner-text p');
 }
 
 function showNavigation(){
@@ -27,4 +34,10 @@ function barsCross(){
     barsEl.click(() => {
         barsEl.toggleClass('cross');
     })
+}
+
+function addClassOn() {
+    bannerTextEl.addClass('on');
+    bannerTextSpanEl.addClass('on');
+    bannerKeywordEl.addClass('on');
 }
