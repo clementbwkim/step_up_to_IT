8498cdfd57bd50473bf1fb49c223575b11c28b4a
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2020-12-01 03:17:32 +0900

    Create landing

--- app/src/js/landing.js
+++ app/src/js/landing.js
@@ -3,13 +3,43 @@ import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+let burgerBtnEl;
+let navEl;
 
+init();
+function init() {
+    initElements();
+    showHideNav();
+    btnCross()
+}
 
-$('.banner-img-wrapper').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+function initElements() {
+  burgerBtnEl = $('.bar-wrapper');
+  navEl = $('.nav-wrapper');
+}
+
+function btnCross() {
+  burgerBtnEl.click(() =>{
+    burgerBtnEl.toggleClass('cross');
   });
+}
 
-  
+function showHideNav() {
+  burgerBtnEl.click(() =>{
+    console.log(123);
+    navEl.toggleClass('header-slide');
+  });
+}
+
+
+$('.banner-items').slick({
+  autoplay : true,
+  dots: true,
+  speed : 300,
+  infinite: true,
+  autoplaySpeed: 3000,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: false
+});
