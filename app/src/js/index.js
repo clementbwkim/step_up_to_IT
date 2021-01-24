2edda07e3f3ddc14f15fc0ee10c5fd22edd747e4
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2021-01-25 02:08:51 +0900

    Update swiper autoplay

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -78,23 +78,10 @@ const slideSetting = {
 		slidePerView: 1,
 		loop: true,
 		speed: 1500,
-    // autoplay: {
-		// 	delay: 3000,
-		// },
-		effect: 'fade',
-
-		// Responsive breakpoints
-		breakpoints: {
-			// when window width is >= 320px
-			320: {
-			},
-			// when window width is >= 480px
-			480: {
-			},
-			// when window width is >= 640px
-			640: {
-			}
+    autoplay: {
+			delay: 3000,
 		},
+		effect: 'fade',
 }
 
 var swiper = new Swiper('.swiper-container', slideSetting);
