546ab38c2bc03c7c542c92ae874c07f7c2809dad
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2021-02-13 03:18:37 +0900

    Create new index

--- app/src/js/index-backup.js
+++ app/src/js/index-backup.js
@@ -1,30 +1,86 @@
+import $ from 'jquery';
+import lazyload from 'lazyload';
+import Swiper from 'swiper/dist/js/swiper.js';
 
-//elements
-let burgerBtnEl;
-let navEl;
+let headerEl;
+let mobileMenuEl;
+let navItemsEl; 
+let scrlItems;
+let winH;
+let currentScrlPos;
+let $window
 
+window.addEventListener('resize', init);
 init();
-
 function init() {
-    initElements(); 
-    initEvents();
+	initElements();
+	showNavigation(mobileMenuEl,navItemsEl);
+	scrollEvent(headerEl);
+	scrollShowNav(headerEl);
+	window.addEventListener('scroll', chkPosTop);
+	window.addEventListener('load', chkPosTop);
 }
 
 function initElements() {
-    burgerBtnEl = document.querySelector('.burger-btn');
-    navEl = document.querySelector('.navigation-wrapper');
-};
-
-function initEvents(){
-    burgerBtnEl.addEventListener('click', toggleHeaderNav);
-    burgerBtnEl.addEventListener('click', crossBar);
+	headerEl = $(".header");
+	mobileMenuEl = $(".mobile-menu"); 
+	navItemsEl = $(".header-nav");
+	scrlItems = $(".scrl-item");	
+	winH = $(window).height();
+	$window = $(window);
 }
 
-//functions
-function toggleHeaderNav(){
-    navEl.classList.toggle('hidden');
-}
-function crossBar(){
-    burgerBtnEl.classList.toggle('cross');
+function showNavigation(el, elm){
+	el.off().on('click',()=>{ //when clicked one time, react one time
+		elm.toggleClass('show')
+	});
+    
 }
 
+function chkPosTop() {
+	for(let i = 0; i < scrlItems.length; i++){
+		let posTop = scrlItems[i].getBoundingClientRect().top;
+		if(winH > posTop) {
+			scrlItems[i].classList.add('active');
+		}
+	}
+}
+
+function scrollEvent(el) {
+	$window.scroll(()=>{
+		currentScrlPos = $window.scrollTop(); //scrollY top
+		let profileSecEl = $('.profile-section').offset().top;
+		if(currentScrlPos > profileSecEl){ //bg color
+			el.addClass('on');
+		}else {
+			el.removeClass('on');
+		}
+	});
+}
+
+function scrollShowNav(el){ //
+	let prevPos = 0; //default
+	if($window.width() > 768){ //if more than width: 768px
+		$window.scroll(()=>{
+			currentScrlPos = $window.scrollTop();
+			el.toggleClass('hidden', currentScrlPos > prevPos); //showin nav 
+			prevPos = currentScrlPos;
+		});
+	}else{
+		$window.scroll(()=>{ //if less than width: 768px
+				el.removeClass('hidden');
+		});
+	}; 
+}
+
+const slideSetting = {
+		slidePerView: 1,
+		loop: true,
+		speed: 1500,
+    autoplay: {
+			delay: 3000,
+		},
+		effect: 'fade',
+}
+
+var swiper = new Swiper('.swiper-container', slideSetting);
--- app/src/js/index.js
+++ app/src/js/index.js
@@ -2,86 +2,18 @@ import $ from 'jquery';
 import lazyload from 'lazyload';
 import Swiper from 'swiper/dist/js/swiper.js';
 
-let headerEl;
-let mobileMenuEl;
-let navItemsEl; 
-let scrlItems;
-let winH;
-let currentScrlPos;
-let $window
-
-window.addEventListener('resize', init);
-init();
-function init() {
-	initElements();
-	showNavigation(mobileMenuEl,navItemsEl);
-	scrollEvent(headerEl);
-	scrollShowNav(headerEl);
-	window.addEventListener('scroll', chkPosTop);
-	window.addEventListener('load', chkPosTop);
-}
-
-function initElements() {
-	headerEl = $(".header");
-	mobileMenuEl = $(".mobile-menu"); 
-	navItemsEl = $(".header-nav");
-	scrlItems = $(".scrl-item");	
-	winH = $(window).height();
-	$window = $(window);
-}
-
-function showNavigation(el, elm){
-	el.off().on('click',()=>{ //when clicked one time, react one time
-		console.log(123);
-		elm.toggleClass('show')
-	});
-    
-}
-
-function chkPosTop() {
-	for(let i = 0; i < scrlItems.length; i++){
-		let posTop = scrlItems[i].getBoundingClientRect().top;
-		if(winH > posTop) {
-			scrlItems[i].classList.add('active');
-		}
-	}
-}
-
-function scrollEvent(el) {
-	$window.scroll(()=>{
-		currentScrlPos = $window.scrollTop(); //scrollY top
-		let profileSecEl = $('.profile-section').offset().top;
-		if(currentScrlPos > profileSecEl){ //bg color
-			el.addClass('on');
-		}else {
-			el.removeClass('on');
-		}
-	});
-}
-
-function scrollShowNav(el){ //
-	let prevPos = 0; //default
-	if($window.width() > 768){ //if more than width: 768px
-		$window.scroll(()=>{
-			currentScrlPos = $window.scrollTop();
-			el.toggleClass('hidden', currentScrlPos > prevPos); //showin nav 
-			prevPos = currentScrlPos;
-		});
-	}else{
-		$window.scroll(()=>{ //if less than width: 768px
-				el.removeClass('hidden');
-		});
-	}; 
-}
-
-const slideSetting = {
-		slidePerView: 1,
+const workSlide = {
+		slidesPerView: 5,
+		spaceBetween: 30,
 		loop: true,
 		speed: 1500,
-    autoplay: {
-			delay: 3000,
+    // autoplay: {
+		// 	delay: 3000,
+		// },
+		navigation: {
+			nextEl: '.slide-next-btn',
+			prevEl: '.slide-prev-btn',
 		},
-		effect: 'fade',
 }
 
-var swiper = new Swiper('.swiper-container', slideSetting);
+var swiper = new Swiper('.swiper-container.work-slider', workSlide);
