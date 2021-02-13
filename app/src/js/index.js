b71e85f602290db897e40ca1a16ca2dd3620e711
clementbwkim <60060205+clementbwkim@users.noreply.github.com>
2021-02-13 20:54:08 +0900

    Create index again

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -2,6 +2,73 @@ import $ from 'jquery';
 import lazyload from 'lazyload';
 import Swiper from 'swiper/dist/js/swiper.js';
 
+let headerEl;
+let greetingTxtEl;
+let positionTxtEl;
+let introTxtEl;
+let menuBtnEl;
+let navListEl;
+let scrlItemsEl;
+let winH;
+let $window;
+
+const scrollYPos = 95;
+
+init();
+function init() {
+	initElements();
+	window.addEventListener('scroll', chkPosTop);
+	loadAddClass(greetingTxtEl);
+	loadAddClass(positionTxtEl);
+	loadAddClass(introTxtEl);
+	scrlEvent();
+	showNav(menuBtnEl, navListEl);
+}
+
+function initElements() {
+	headerEl = document.querySelector('.header')
+	greetingTxtEl = document.querySelector('.txt-greeting');
+	positionTxtEl = document.querySelector('.txt-position');
+	introTxtEl = document.querySelector('.txt-intro');
+	menuBtnEl = document.querySelector('.menu-button');
+	navListEl = document.querySelector('.header-nav');
+	scrlItemsEl = document.querySelectorAll('.scrl-item');
+	winH = window.innerHeight;
+	$window = $(window);
+}
+
+function loadAddClass(el) {
+	window.addEventListener('load', ()=>{
+		el.classList.add('txt-on');	
+	});
+}
+
+function showNav(el, elm) {
+	el.addEventListener('click', () => {
+		el.classList.toggle('cross');
+		elm.classList.toggle('show');
+	});
+}
+
+function chkPosTop() {
+	for(let i = 0; i < scrlItemsEl.length; i++) { 
+		let posTop = scrlItemsEl[i].getBoundingClientRect().top;
+		if(winH > posTop) {
+			scrlItemsEl[i].classList.add('active');
+		}
+	}
+}
+
+function scrlEvent() {
+	window.addEventListener('scroll', () => {
+		if(window.pageYOffset> scrollYPos) {
+			headerEl.classList.add('on');
+		}else {
+			headerEl.classList.remove('on');
+		}
+	})
+}
+
 const workSlide = {
 		slidesPerView: 5,
 		spaceBetween: 30,
@@ -14,6 +81,20 @@ const workSlide = {
 			nextEl: '.slide-next-btn',
 			prevEl: '.slide-prev-btn',
 		},
+		breakpoints: {
+			375: {
+				slidesPerView: 1,
+			},
+			575: {
+				slidesPerView: 2,
+			},
+			1024: {
+				slidesPerView: 3,
+			},
+			1400: {
+				slidesPerView: 4,
+			},
+		}
 }
 
 var swiper = new Swiper('.swiper-container.work-slider', workSlide);
